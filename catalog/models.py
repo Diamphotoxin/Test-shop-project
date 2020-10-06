222ebc80e94b4c140d8b8b6ec99e8a490462cd4d
Diamphotoxin <seleznevavld@gmail.com>
2020-10-06 17:10:14 +0300

    added models

---
+++ catalog/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.core.validators import RegexValidator, MaxLengthValidator
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    img = models.ImageField(null=True, blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    telephone = RegexValidator(r'^\d+$', 'Only numeric characters are allowed.')
+
+    customer_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=50)
+    phone = models.CharField(max_length=15, validators=[telephone, MaxLengthValidator])
+    items = models.ManyToManyField('Item')
+
+
+class Item(models.Model):
+    quantity = models.PositiveIntegerField(default=0)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
