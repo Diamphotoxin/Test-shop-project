ce73b2d0dc97378161ce82742de0a5962f74cb36
Diamphotoxin <seleznevavld@gmail.com>
2020-10-19 14:02:17 +0300

    auxiliary files

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Order, Product, Category, Item
 
-# Register your models here.
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(Category)
+admin.site.register(Item)
+
---
+++ shop/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path
+from catalog.views import ProductListView, ItemListView, OrderListView, ProductDetailView, SuccessListView
+
+from django.conf.urls.static import static, settings
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', ProductListView.as_view(), name='home'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='detail'),
+    path('order/', OrderListView.as_view(), name='order'),
+    path('success/', SuccessListView.as_view(), name='success'),
+    path('cart/', ItemListView.as_view(), name='cart'),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
