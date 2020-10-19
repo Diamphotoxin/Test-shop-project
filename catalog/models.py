f7d190530e04856e0ac8f514c4e51cd0a6ab9a77
Diamphotoxin <seleznevavld@gmail.com>
2020-10-19 13:55:41 +0300

    updated models

--- catalog/models.py
+++ catalog/models.py
@@ -7,7 +7,7 @@ class Product(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
     img = models.ImageField(null=True, blank=True)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
 
     def __str__(self):
         return self.name
@@ -21,11 +21,20 @@ class Order(models.Model):
     phone = models.CharField(max_length=15, validators=[telephone, MaxLengthValidator])
     items = models.ManyToManyField('Item')
 
+    def __str__(self):
+        return self.id
+
 
 class Item(models.Model):
-    quantity = models.PositiveIntegerField(default=0)
+    quantity = models.PositiveIntegerField(default=1)
     product = models.ForeignKey('Product', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.id
 
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
